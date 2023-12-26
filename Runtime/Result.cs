f50cbfbc7cc578bec052bb19be529a98d8e7a285
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-12-26 16:24:11 +0300

    Add direction, Result and ValueResult

---
+++ Runtime/Result.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace KiuhTools
+{
+    public abstract class Result
+    {
+        public bool Success { get; protected set; }
+        public bool Failure => !Success;
+
+        protected string Err;
+
+        public string Error =>
+            Failure
+                ? Err
+                : throw new Exception(
+                    $"You can't access .{nameof(Error)} when .{nameof(Failure)} is false"
+                );
+    }
+
+    public class SuccessResult : Result
+    {
+        public SuccessResult()
+        {
+            Success = true;
+        }
+    }
+
+    public class FailResult : Result
+    {
+        public FailResult(string error)
+        {
+            Success = false;
+            base.Err = error;
+        }
+    }
+}
